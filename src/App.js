0e7db6a60a902ee846089a8f639202c461cfe2fe
Stem Stem <stem@astanait.edu.kz>
2025-02-26 10:25:37 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; 
+import ToDoList from './ToDoList'; 
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
